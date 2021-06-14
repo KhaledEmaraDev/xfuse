f1010f0430120f08aac33d818574eec515e50875
Alan Somers <asomers@gmail.com>
2021-06-14 17:11:34 -0600

    Add basic CLI parsing with Clap

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,42 @@ mod libxfuse;
 
 use libxfuse::volume::Volume;
 
+use clap::crate_version;
 use fuse::mount;
+use std::ffi::{OsString, OsStr};
 
 fn main() {
-    let device = match std::env::args().nth(1) {
-        Some(path) => path,
-        None => {
-            eprintln!(
-                "Usage: {} <DEVICE> <MOUNTPOINT>.  Missing mountpoint argument",
-                std::env::args().nth(0).unwrap()
-            );
-            return;
-        }
-    };
+    let app = clap::App::new("xfs-fuse")
+        .version(crate_version!())
+        .arg(clap::Arg::with_name("option")
+             .help("Mount options")
+             .short("o")
+             .takes_value(true)
+             .multiple(true)
+             .require_delimiter(true)
+        ).arg(clap::Arg::with_name("device")
+             .required(true)
+        ).arg(clap::Arg::with_name("mountpoint")
+             .required(true)
+        );
+    let matches = app.get_matches();
 
-    let mountpoint = match std::env::args().nth(2) {
-        Some(path) => path,
-        None => {
-            eprintln!(
-                "Usage: {} <DEVICE> <MOUNTPOINT>.  Missing mountpoint argument",
-                std::env::args().nth(0).unwrap()
-            );
-            return;
+    let mut opts = Vec::new();
+    if let Some(it) = matches.values_of("option") {
+        for o in it {
+            // mount_fusefs expects to have a separate "-o" per option
+            opts.push(OsString::from("-o"));
+            opts.push(OsString::from(o));
         }
     };
+    // We need a separate vec of references :(
+    // https://github.com/zargony/rust-fuse/issues/117
+    let opt_refs = opts.iter().map(|o| o.as_ref()).collect::<Vec<&OsStr>>();
+
+    let device = matches.value_of("device").unwrap().to_string();
+    let mountpoint = matches.value_of("mountpoint").unwrap().to_string();
 
     let vol = Volume::from(&device);
 
-    mount(vol, &mountpoint, &[]).unwrap();
+    mount(vol, &mountpoint, &opt_refs[..]).unwrap();
 }
