adacc8a29eea4028fae87d47986956565f5f201e
Khaled Emara <mail@KhaledEmara.dev>
2021-07-12 17:59:18 +0200

    Implement Lookup for Btree Directories

--- src/libxfuse/dir3_bptree.rs
+++ src/libxfuse/dir3_bptree.rs
@@ -4,15 +4,19 @@ use std::mem;
 
 use super::bmbt_rec::BmbtRec;
 use super::btree::BtreeBlock;
+use super::da_btree::{hashname, XfsDa3NodeEntry, XfsDa3NodeHdr};
 use super::definitions::*;
+use super::dinode::Dinode;
 use super::dir3::{
-    Dir2DataEntry, Dir2DataUnused, Dir3, Dir3DataHdr, XFS_DIR3_FT_DIR, XFS_DIR3_FT_REG_FILE,
+    Dir2DataEntry, Dir2DataUnused, Dir2LeafEntry, Dir3, Dir3DataHdr, Dir3LeafHdr, XFS_DIR3_FT_DIR,
+    XFS_DIR3_FT_REG_FILE,
 };
 use super::sb::Sb;
 
 use byteorder::{BigEndian, ReadBytesExt};
 use fuse::{FileAttr, FileType};
-use libc::{c_int, ENOENT};
+use libc::{c_int, mode_t, ENOENT, S_IFMT};
+use time::Timespec;
 
 #[derive(Debug, Clone)]
 pub struct BmdrBlock {
@@ -71,43 +75,18 @@ impl Dir2Btree {
             block_size,
         }
     }
-}
 
-impl Dir3 for Dir2Btree {
-    fn lookup<T: BufRead + Seek>(
+    pub fn map_dblock<R: BufRead + Seek>(
         &self,
-        _buf_reader: &mut T,
-        _super_block: &Sb,
-        _name: &str,
-    ) -> Result<(FileAttr, u64), c_int> {
-        todo!();
-    }
-
-    fn next<T: BufRead + Seek>(
-        &self,
-        buf_reader: &mut T,
-        offset: i64,
-    ) -> Result<(XfsIno, i64, FileType, String), c_int> {
-        let offset = offset as u64;
-        let idx = offset >> (64 - 48); // tags take 16-bits
-        let offset = offset & ((1 << (64 - 48)) - 1);
-
-        let mut next = offset == 0;
-        let mut offset = if offset == 0 {
-            mem::size_of::<Dir3DataHdr>() as u64
-        } else {
-            offset
-        };
-
+        buf_reader: &mut R,
+        dblock: XfsDablk,
+    ) -> (Option<XfsBmbtBlock>, Option<BmbtRec>) {
         let mut bmbt: Option<XfsBmbtBlock> = None;
         let mut bmbt_rec: Option<BmbtRec> = None;
         let mut bmbt_block_offset = 0;
-        let mut bmbt_rec_idx = 0;
 
-        for (i, BmbtKey { br_startoff: key }) in self.keys.iter().rev().enumerate() {
-            if idx >= *key {
-                let i = self.keys.len() - 1 - i;
-
+        for (i, BmbtKey { br_startoff: key }) in self.keys.iter().enumerate().rev() {
+            if dblock as u64 >= *key {
                 bmbt_block_offset = self.pointers[i] * (self.block_size as u64);
                 buf_reader.seek(SeekFrom::Start(bmbt_block_offset)).unwrap();
 
@@ -137,7 +116,7 @@ impl Dir3 for Dir2Btree {
                     .unwrap();
                 let key = BmbtKey::from(buf_reader.by_ref()).br_startoff;
 
-                match key.cmp(&idx) {
+                match key.cmp(&dblock.into()) {
                     Ordering::Greater => {
                         r = m - 1;
                     }
@@ -185,7 +164,7 @@ impl Dir3 for Dir2Btree {
                     .unwrap();
                 let key = BmbtRec::from(buf_reader.by_ref()).br_startoff;
 
-                match key.cmp(&idx) {
+                match key.cmp(&dblock.into()) {
                     Ordering::Greater => {
                         r = m - 1;
                     }
@@ -208,13 +187,158 @@ impl Dir3 for Dir2Btree {
                 ))
                 .unwrap();
             bmbt_rec = Some(BmbtRec::from(buf_reader.by_ref()));
+        }
 
-            if let Some(bmbt_rec_some) = &bmbt_rec {
-                bmbt_block_offset = buf_reader.stream_position().unwrap();
-                bmbt_rec_idx = idx - bmbt_rec_some.br_startoff;
-            } else {
-                return Err(ENOENT);
+        return (bmbt, bmbt_rec);
+    }
+}
+
+impl Dir3 for Dir2Btree {
+    fn lookup<T: BufRead + Seek>(
+        &self,
+        buf_reader: &mut T,
+        super_block: &Sb,
+        name: &str,
+    ) -> Result<(FileAttr, u64), c_int> {
+        let idx = super_block.get_dir3_leaf_offset();
+        let hash = hashname(name);
+
+        let (_, bmbt_rec) = self.map_dblock(buf_reader.by_ref(), idx as u32);
+        let mut hdr: Option<XfsDa3NodeHdr> = None;
+
+        if let Some(bmbt_rec_some) = &bmbt_rec {
+            buf_reader
+                .seek(SeekFrom::Start(
+                    (bmbt_rec_some.br_startblock) * (self.block_size as u64),
+                ))
+                .unwrap();
+
+            hdr = Some(XfsDa3NodeHdr::from(buf_reader.by_ref()));
+
+            while let Some(hdr_some) = &hdr {
+                loop {
+                    let entry = XfsDa3NodeEntry::from(buf_reader.by_ref());
+                    if entry.hashval > hash {
+                        let (_, bmbt_rec) = self.map_dblock(buf_reader.by_ref(), entry.before);
+
+                        if let Some(bmbt_rec_some) = &bmbt_rec {
+                            buf_reader
+                                .seek(SeekFrom::Start(
+                                    (bmbt_rec_some.br_startblock) * (self.block_size as u64),
+                                ))
+                                .unwrap();
+
+                            break;
+                        } else {
+                            return Err(ENOENT);
+                        }
+                    }
+                }
+
+                if hdr_some.level == 1 {
+                    break;
+                } else {
+                    hdr = Some(XfsDa3NodeHdr::from(buf_reader.by_ref()));
+                }
             }
+        } else {
+            return Err(ENOENT);
+        }
+
+        let hdr = Dir3LeafHdr::from(buf_reader.by_ref());
+
+        for _i in 0..hdr.count {
+            let entry = Dir2LeafEntry::from(buf_reader.by_ref());
+
+            if entry.hashval == hash {
+                let address = (entry.address as u64) * 8;
+                let idx = (address / (self.block_size as u64)) as usize;
+                let address = address % (self.block_size as u64);
+
+                let (_, bmbt_rec) = self.map_dblock(buf_reader.by_ref(), idx as u32);
+
+                if let Some(bmbt_rec_some) = &bmbt_rec {
+                    buf_reader
+                        .seek(SeekFrom::Start(
+                            (bmbt_rec_some.br_startblock) * (self.block_size as u64),
+                        ))
+                        .unwrap();
+
+                    buf_reader.seek(SeekFrom::Current(address as i64)).unwrap();
+
+                    let entry = Dir2DataEntry::from(buf_reader.by_ref());
+
+                    let dinode = Dinode::from(buf_reader.by_ref(), &super_block, entry.inumber);
+
+                    let kind = match (dinode.di_core.di_mode as mode_t) & S_IFMT {
+                        S_IFREG => FileType::RegularFile,
+                        S_IFDIR => FileType::Directory,
+                        _ => {
+                            return Err(ENOENT);
+                        }
+                    };
+
+                    let attr = FileAttr {
+                        ino: entry.inumber,
+                        size: dinode.di_core.di_size as u64,
+                        blocks: dinode.di_core.di_nblocks,
+                        atime: Timespec {
+                            sec: dinode.di_core.di_atime.t_sec as i64,
+                            nsec: dinode.di_core.di_atime.t_nsec,
+                        },
+                        mtime: Timespec {
+                            sec: dinode.di_core.di_mtime.t_sec as i64,
+                            nsec: dinode.di_core.di_mtime.t_nsec,
+                        },
+                        ctime: Timespec {
+                            sec: dinode.di_core.di_ctime.t_sec as i64,
+                            nsec: dinode.di_core.di_ctime.t_nsec,
+                        },
+                        crtime: Timespec { sec: 0, nsec: 0 },
+                        kind,
+                        perm: dinode.di_core.di_mode & (!(S_IFMT as u16)),
+                        nlink: dinode.di_core.di_nlink,
+                        uid: dinode.di_core.di_uid,
+                        gid: dinode.di_core.di_gid,
+                        rdev: 0,
+                        flags: 0,
+                    };
+
+                    return Ok((attr, dinode.di_core.di_gen.into()));
+                } else {
+                    return Err(ENOENT);
+                };
+            }
+        }
+
+        Err(ENOENT)
+    }
+
+    fn next<T: BufRead + Seek>(
+        &self,
+        buf_reader: &mut T,
+        offset: i64,
+    ) -> Result<(XfsIno, i64, FileType, String), c_int> {
+        let offset = offset as u64;
+        let idx = offset >> (64 - 48); // tags take 16-bits
+        let offset = offset & ((1 << (64 - 48)) - 1);
+
+        let mut next = offset == 0;
+        let mut offset = if offset == 0 {
+            mem::size_of::<Dir3DataHdr>() as u64
+        } else {
+            offset
+        };
+
+        let (mut bmbt, mut bmbt_rec) = self.map_dblock(buf_reader.by_ref(), idx as u32);
+        let mut bmbt_block_offset = 0;
+        let mut bmbt_rec_idx = 0;
+
+        if let Some(bmbt_rec_some) = &bmbt_rec {
+            bmbt_block_offset = buf_reader.stream_position().unwrap();
+            bmbt_rec_idx = idx - bmbt_rec_some.br_startoff;
+        } else {
+            return Err(ENOENT);
         }
 
         while let Some(bmbt_some) = &bmbt {
