b29e3f03a2cfb858efc0b585c65cc9d888542076
Alan Somers <asomers@gmail.com>
2024-01-26 15:10:59 -0700

    Write tests for reading plain files
    And fix a crash when attempting to read past EoF

--- scripts/mkimg.sh
+++ scripts/mkimg.sh
@@ -56,6 +56,34 @@ python3 -c "import socket as s; sock = s.socket(s.AF_UNIX); sock.bind('${MNTDIR}
 mknod ${MNTDIR}/files/blockdev b 1 2
 mknod ${MNTDIR}/files/chardev c 1 2
 
+# Now create some files that contain data.  Fill each file with an array of
+# 16-byte ASCII strings.  Each string contains the address, in ASCII, of its
+# starting position.  Use ASCII because it's easy to create from a shell
+# script.
+jot -n -w %016x -s "" 5 0 80 16 > ${MNTDIR}/files/single_extent.txt
+
+# Use fallocate's collapse function to force the file to be allocated in
+# multiple small extents, rather than one big one.
+jot -n -w %016x -s "" 256 0 4096 16 > ${MNTDIR}/files/four_extents.txt
+jot -n -b X -s "" 4096 0 >> ${MNTDIR}/files/four_extents.txt
+jot -n -w %016x -s "" 256 4096 8192 16 >> ${MNTDIR}/files/four_extents.txt
+jot -n -b X -s "" 4096 0 >> ${MNTDIR}/files/four_extents.txt
+jot -n -w %016x -s "" 256 8192 12288 16 >> ${MNTDIR}/files/four_extents.txt
+jot -n -b X -s "" 4096 0 >> ${MNTDIR}/files/four_extents.txt
+jot -n -w %016x -s "" 256 12288 16384 16 >> ${MNTDIR}/files/four_extents.txt
+fallocate -c -o 4096 -l 4096 ${MNTDIR}/files/four_extents.txt
+fallocate -c -o 8192 -l 4096 ${MNTDIR}/files/four_extents.txt
+fallocate -c -o 12288 -l 4096 ${MNTDIR}/files/four_extents.txt
+
+for i in `seq 0 16`; do
+	jot -n -w %016x -s "" 256 $(( i * 4096 )) $(( ( $i + 1 ) * 4096 )) 16 >> ${MNTDIR}/files/btree.txt
+	jot -n -b X -s "" 4096 0 >> ${MNTDIR}/files/btree.txt
+done
+for i in `seq 0 15`; do
+	fallocate -c -o $(( ( $i + 1 ) * 4096 )) -l 4096 ${MNTDIR}/files/btree.txt
+done
+ 
+
 umount ${MNTDIR}
 
 rmdir $MNTDIR
